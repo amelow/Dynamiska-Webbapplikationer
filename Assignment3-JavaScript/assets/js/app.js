997ccb6b002637779d53552ec35e95966009527e
amelow <amelielowe96@gmail.com>
2019-05-02 11:28:00 +0200

    fetching the data from the server
    Getting the data and printing the result in the new handleResult
    function

--- Assignment3-JavaScript/assets/js/app.js
+++ Assignment3-JavaScript/assets/js/app.js
@@ -1,5 +1,15 @@
 'use strict'
 function init () {
-   
+    console.log("Initializing program");
+   fetchData();
   }
+  function fetchData(url = 'https://stryk.herokuapp.com/tipset'){
+    fetch(url)
+    .then(result => result.json())
+    .then(result => handleResult(result))
+    .catch(error => console.log(error))
+  }
+  function handleResult (result) {
+      console.log(result);
+  }    
   window.addEventListener('load', init) 
