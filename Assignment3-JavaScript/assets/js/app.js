2cfd10bc4714d678d4ec410093e3171ad7da7ba2
amelow <amelielowe96@gmail.com>
2019-05-02 19:47:24 +0200

    getting started with the innerHtml

--- Assignment3-JavaScript/assets/js/app.js
+++ Assignment3-JavaScript/assets/js/app.js
@@ -1,15 +1,26 @@
 'use strict'
-function init () {
+function init() {
     console.log("Initializing program");
-   fetchData();
-  }
-  function fetchData(url = 'https://stryk.herokuapp.com/tipset'){
+    fetchData();
+}
+function fetchData(url = 'https://stryk.herokuapp.com/tipset') {
     fetch(url)
-    .then(result => result.json())
-    .then(result => handleResult(result))
-    .catch(error => console.log(error))
-  }
-  function handleResult (result) {
-      console.log(result);
-  }    
-  window.addEventListener('load', init) 
+        .then(result => result.json())
+        .then(result => handleResult(result))
+        .catch(error => console.log(error))
+}
+function handleResult(result) {
+    console.log("In handleResult function");
+    console.log("All results:");
+    for(let i = 0; i<=13;i++){
+        var trElement = document.createElement("tr")
+        document.body.appendChild(trElement);  
+        trElement.innerHTML = "TEST";  
+        console.log(result[i]);
+    }
+    
+    //console.log(result[0].gameNumber);
+   
+    
+}
+window.addEventListener('load', init) 
