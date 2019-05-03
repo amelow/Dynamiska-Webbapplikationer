aac331edf0f2bf6afb9447b1c02f4c01cefd7c40
amelow <amelielowe96@gmail.com>
2019-05-03 11:36:55 +0200

    creating the td and tr elements for the teams

--- Assignment3-JavaScript/assets/js/app.js
+++ Assignment3-JavaScript/assets/js/app.js
@@ -12,15 +12,25 @@ function fetchData(url = 'https://stryk.herokuapp.com/tipset') {
 function handleResult(result) {
     console.log("In handleResult function");
     console.log("All results:");
-    for(let i = 0; i<=13;i++){
-        var trElement = document.createElement("tr")
-        document.body.appendChild(trElement);  
-        trElement.innerHTML = "TEST";  
+    console.log(result);
+
+   for (let i = 0; i <= result.length; ++i) {
+        var numberElements = document.createElement("tr");
+        var teamElements = document.createElement("td");
+        numberElements.setAttribute("id", "rowrow"); 
+        numberElements.innerHTML =result[i].gameNumber;
+        teamElements.innerHTML =result[i].teams;
+        
+        document.getElementById("table").appendChild(numberElements);
+        document.getElementById("rowrow").appendChild(teamElements);
+       // document.getElementById("table").appendChild(teamElements);
         console.log(result[i]);
+
     }
-    
+
     //console.log(result[0].gameNumber);
-   
-    
+
+
 }
+
 window.addEventListener('load', init) 
