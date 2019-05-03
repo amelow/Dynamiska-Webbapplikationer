12219b57519530c181aba1e7e9adcdb2ccec09d0
amelow <amelielowe96@gmail.com>
2019-05-03 12:14:09 +0200

    correcting a mistake
    Creating one tr element per each row then creating 3 td elements in
    each tr element for the the columns of information

--- Assignment3-JavaScript/assets/js/app.js
+++ Assignment3-JavaScript/assets/js/app.js
@@ -14,21 +14,56 @@ function handleResult(result) {
     console.log("All results:");
     console.log(result);
 
-   for (let i = 0; i <= result.length; ++i) {
-        var numberElements = document.createElement("tr");
+   for (let i = 0; i <= result.length;i++) {
+       var trElements = document.createElement("tr");
+       trElements.setAttribute("id","Rows");
+       document.getElementById("table").appendChild(trElements);
+      
+        var numbers= document.createElement("td");
+        numbers.setAttribute("id", "Num");
+        numbers.innerHTML =result[i].gameNumber;
+        trElements.appendChild(numbers);
+
+        var teams = document.createElement("td");
+        teams.setAttribute("id","Team");
+        teams.innerHTML = result[i].teams;
+        trElements.appendChild(teams);
+
+        var checkmark = document.createElement("td");
+       
+      
+       
+
+
+
+  /*  var numberElements = document.createElement("tr");
+    var teamElements = document.createElement("td");
+    numberElements.setAttribute("id", "num"); 
+    numberElements.innerHTML =result[i].gameNumber;
+    teamElements.innerHTML =result[i].teams;
+    document.getElementById("table").appendChild(numberElements);
+    document.getElementById("num").appendChild(teamElements);
+    */
+
+
+      /* var trElements = document.createElement("tr");
+       trElements.setAttribute("id", "trEle"); 
+
+        var numberElements = document.createElement("td");
         var teamElements = document.createElement("td");
-        numberElements.setAttribute("id", "rowrow"); 
+        numberElements.setAttribute("id", "num"); 
         numberElements.innerHTML =result[i].gameNumber;
         teamElements.innerHTML =result[i].teams;
-        
-        document.getElementById("table").appendChild(numberElements);
-        document.getElementById("rowrow").appendChild(teamElements);
-       // document.getElementById("table").appendChild(teamElements);
+        document.getElementById("trEle").appendChildren(numberElements,teamElements);
+        document.getElementById("table").appendChild(trElements);
+
+       // document.getElementById("table").appendChild(numberElements);
+        //document.getElementById("num").appendChild(teamElements);
+        */
         console.log(result[i]);
 
     }
 
-    //console.log(result[0].gameNumber);
 
 
 }
